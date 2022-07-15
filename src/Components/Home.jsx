e43af344b64d962b988365c64a0a798a2f944d88
huntergroff <groff.h@northeastern.edu>
2022-07-15 14:04:46 -0400

    Working on home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import '../Styles/Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <div>hello?</div>
-      <div>hello?</div>
-      <div>hello?</div>
-      <div>hello?</div>
-      <div>hello?</div>
-      <div>Home?</div>
-    </div>
+    <>
+      <div className='bg'></div>
+      <div className='container'>
+        <div className='home-title'>
+          <p>welcome to</p>
+          <h1>Synergy</h1>
+          <h2>Center for Dance and the<br></br>Performing Arts</h2>
+        </div>
+        <div className='home-mia-message'>
+        </div> 
+      </div>
+    </>
   )
 }
 
