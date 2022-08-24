790fb548fd5917e27e0d66884aefbf7834d645db
huntergroff <groff.h@northeastern.edu>
2022-08-24 10:59:06 -0400

    fixing safari formatting again

--- src/Components/Pages/ComingSoon.jsx
+++ src/Components/Pages/ComingSoon.jsx
@@ -6,7 +6,7 @@ import '../../Styles/ComingSoon.css'
 
 const ComingSoon = () => {
   return (
-    <div className='coming-soon container'>
+    <div className='coming-soon'>
         <h1>This page is coming soon!</h1>
         <p>Our new website is still under construction. Please check back soon for updates. Thanks for your patience!</p>
         <Link to="/">Return Home</Link>
