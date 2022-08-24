40f4315a38cd7ee90bd1a09a17634911b8532894
huntergroff <groff.h@northeastern.edu>
2022-08-24 10:20:09 -0400

    Adjusted mobile menu to handle external links

--- src/Components/Header/MobileMenuLink.jsx
+++ src/Components/Header/MobileMenuLink.jsx
@@ -13,19 +13,35 @@ const MobileMenuLink = ({ menu, toggleMenu }) => {
   const [open, setOpen] = useState(false);
   const toggleOpen = () => setOpen(!open);
 
+  let menuListItem;
+
+  /** If the menu has a submenu,  create dropdown with arrow */
+  if (menu.submenu) {
+    menuListItem = (
+      <>
+        <div className='dropdown-item'onClick={toggleOpen}>
+          <span>{menu.title}</span>
+          <BiChevronDown className={`arrow ${open ? 'open' : ''}`}/>
+          </div>
+        <MobileDropdown submenus={menu.submenu} isOpen={open} toggleMenu={toggleMenu}/>
+      </>
+    )
+  /** else if the menu is an external link, create an a tag to the external site */
+  } else if (menu.external) {
+    menuListItem = (
+      <a href={menu.link} target="_blank" rel="noreferrer">{menu.title}</a>
+    )
+
+  /** else if the menu has an internal link, render a Link to that page of the site */
+  } else {
+    menuListItem = (
+      <Link to={menu.link} onClick={toggleMenu}>{menu.title}</Link>
+    )  
+  }
+
   return (
       <li className='link'>
-        {menu.submenu ? 
-          <>
-            <div className='dropdown-item'onClick={toggleOpen}>
-              <span>{menu.title}</span>
-              <BiChevronDown className={`arrow ${open ? 'open' : ''}`}/>
-              </div>
-            <MobileDropdown submenus={menu.submenu} isOpen={open} toggleMenu={toggleMenu}/>
-          </>
-          : 
-          <Link to={menu.link} onClick={toggleMenu}>{menu.title}</Link>
-        }
+        {menuListItem}
       </li>
   )
 }
