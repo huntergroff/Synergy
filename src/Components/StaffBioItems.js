6fb2dd674239f816e21cd13ea9a8740a2ce52af4
popsrdg <popsrdg@gmail.com>
2025-10-20 19:53:54 -0400

    teach bios and annual calendar updated

--- src/Components/StaffBioItems.js
+++ src/Components/StaffBioItems.js
@@ -14,29 +14,31 @@
 
 //Images - add your import statement here
 import React from 'react';
-import AlexBacheller from '../Images/Staff/alex-bacheller.jpg';
+//import AlexBacheller from '../Images/Staff/alex-bacheller.jpg';
 import AmandaBiggs from '../Images/Staff/amanda-biggs.jpg';
-import AprilFoster from '../Images/Staff/april-foster.jpg';
+import AnnaGonzales from '../Images/Staff/anna-gonzalez.jpg';
+//import AprilFoster from '../Images/Staff/april-foster.jpg';
 import BreaMcBride from '../Images/Staff/brea-mcbride.jpg';
-import ChristanaDavis from '../Images/Staff/christana-davis.jpg';
+//import ChristanaDavis from '../Images/Staff/christana-davis.jpg';
 import EmilyBrace from '../Images/Staff/emily-brace.jpg';
+import ErinThompson from '../Images/Staff/erin-thompson.jpg';
 import EllaDavis from '../Images/Staff/ella-davis.jpg';
-import EllaLavoie from '../Images/Staff/ella-lavoie.jpg';
-import EmmaMontgomery from '../Images/Staff/emma-montgomery.jpg';
+//import EllaLavoie from '../Images/Staff/ella-lavoie.jpg';
+//import EmmaMontgomery from '../Images/Staff/emma-montgomery.jpg';
 import GabbyCowan from '../Images/Staff/gabby-cowan.jpg';
 import JacquelineFeiss from '../Images/Staff/jacqueline-feiss.jpg';
-import JadaBean from '../Images/Staff/jada-bean.jpg';
+//import JadaBean from '../Images/Staff/jada-bean.jpg';
 import MiaGroff from '../Images/Staff/mia-groff.jpg';
-import NaiaSurks from '../Images/Staff/naia-surks.jpg';
+//import NaiaSurks from '../Images/Staff/naia-surks.jpg';
 import PilarMedina from '../Images/Staff/pilar-medina.jpg';
 import RoseBedard from '../Images/Staff/rose-bedard.jpg';
-import RosalieKosnitsky from '../Images/Staff/rosalie-kosnitsky.jpg';
-import TannerPaffen from '../Images/Staff/tanner-paffen.jpg';
+//import RosalieKosnitsky from '../Images/Staff/rosalie-kosnitsky.jpg';
+//import TannerPaffen from '../Images/Staff/tanner-paffen.jpg';
 import TaylorGernander from '../Images/Staff/taylor-gernander.jpg';
 import TheresaRobinson from '../Images/Staff/theresa-robinson.jpg';
-import ZoePapaseraphim from '../Images/Staff/zoe-papaseraphim.jpg';
-import ChatchPregger from '../Images/Staff/chatch-pregger.jpg';
-import SummerClough from '../Images/Staff/summer-clough.jpg';
+//import ZoePapaseraphim from '../Images/Staff/zoe-papaseraphim.jpg';
+//import ChatchPregger from '../Images/Staff/chatch-pregger.jpg';
+//import SummerClough from '../Images/Staff/summer-clough.jpg';
 
 import NoPicGirl from '../Images/Staff/no-teacher-pic-girl.jpg';
 /*import NoPicBoy from '../Images/Staff/no-teacher-pic-boy.jpg';*/
@@ -54,9 +56,19 @@ export const TeacherBioItems = [
         bio: "Amanda grew up in Maryland, and started dancing, competing, and performing when she was 4. She fell in love with performing as a child, and earned her first degree in Theatre Arts with a concentration in Dance. Amanda danced professionally for a modern dance company in Washington D.C., and started teaching jazz and creative movement before moving to Vermont. Amanda now holds an additional Bachelor’s Degree in Elementary Education and has been teaching dance for 19 years to students of all ages. As an experienced teacher, accomplished dancer, and now, mother of two young children, she loves sharing the joy of dance by offering Creative Movement, Jazz, and Hip Hop to the youngest dancers in our community."
     },
     {
+        name: "Anna Gonzales",
+        picture: <img src={AnnaGonzales} alt="Anna Gonzales" />,
+        bio: "Anna began dancing in a wide variety of styles with Southern Dance Theater in South Florida, building a strong foundation in ballet under Julie Bravata. She found a love and focus for classical ballet, and went on to attend the Harid Conservatory during high school, where she was immersed in Vaganova Ballet training. From there, she became a member of Milwaukee Ballet II, participating in outreach performances, and performing alongside her fellow company members and the main company. She attended school at the University of Florida whilst dancing with Roanoke Ballet Theater and Dance Alive National Ballet. Anna has had the privilege to dance in classical productions of Swan Lake, A Midsummer Night’s Dream, Serenade, A Birthday Offering, and the Nutcracker of course! She has performed in original contemporary works, which have been equally exciting to perform. Once graduated, she made the move to Vermont and was able to teach Jazz and Contemporary classes with Vermont Ballet Theater, also acting as a supportive staff and substitute teacher for ballet.  Anna loves imparting the artistic and athletic sides of dance through teaching, and is very excited to delve into another year of sharing this artform with students."
+    },
+    {
         name: "Brea McBride",
         picture: <img src={BreaMcBride} alt="Brea McBride" />,
         bio: "Brea McBride is the proud owner and Artistic Director of Infinity Dance Studio in Essex Junction, VT.  She credits much of her success in opening the business to the completion of the Start-Up Course offered through the Women's Small Business Program.  Brea began her pre-professional training in her hometown of Ironton, Ohio at Yvonne Dekay’s School of Dance.  She has also lived and furthered her dance education in New Orleans, San Francisco, and New York City.  After graduating from the American Musical and Dramatic Academy in NYC, she was hired as a Mainstage Performer for Disney Cruise Lines.  Brea began her career as a dance educator at The Cynthia King Dance Studio in Brooklyn, NY.  She has taught and choreographed for several local VT studios since moving here in 2004.  Proud moments include restaging a children’s play for the Shelburne Players, choreographing a piece for the Haunted Forest, and working with the children at Camp Dream and Camp Kiniya.  Her most recent professional experience as a principal dancer in Ballet Vermont’s production of Bees and Friends was incredibly rewarding.  Brea is thrilled to have the opportunity to join the Synergy faculty!"
+    },
+    {
+        name: "Ella Davis",
+        picture: <img src={EllaDavis} alt="Ella Davis" />,
+        bio: ""
     },
     {
         name: "Emily Brace",
@@ -64,14 +76,19 @@ export const TeacherBioItems = [
         bio: "Emily is a twin girl mama, preschool teacher, plant eater, headstand enthusiast, and has been dancing for nearly 25 years. Emily has choreographed for the Johnson State College Dance Ensemble, performed in two large productions with the Vermont Dance Collective, danced a season as a soloist with the VanDenCross Dance Company, and taught at the Birds Nest for four years. She is excited to dance with you!"
     },
     {
-        name: "Emma Montgomery",
-        picture: <img src={EmmaMontgomery} alt="Emma Montgomery" />,
-        bio: ""
+        name: "Erin Thompson",
+        picture: <img src={ErinThompson} alt="Erin Thompson" />,
+        bio: "Erin Thompson is the Founder and Producing Artistic Director of The Upper Peninsula Summer Theater Institute (an educational summer musical theater program), as well as an award-winning Director, Choreographer, Equity Performer, and Theatre Educator. She hails from the Midwest, and recently moved to VT after 13 years of teaching and performing in New York City. Erin received a BFA in Musical Theater from The Boston Conservatory of Music in 2003. She spent the next 15 years performing professionally in commercial musical theater at nationally renowned regional theaters all over the country. Amidst her experience on over 40 professional shows, she served as Dance Captain and Assistant/Associate Director/Choreographer to notable artistic and production teams on several of the shows, which would groom her for her latter career as a Director-Choreographer. She draws on her many years of professional experience onstage to bring empathy to her leadership in the rehearsal room. She most recently earned an MFA in Performing Arts Management from Brooklyn College (class of 2023), where she also served as a Teaching Fellow to undergraduate students. Before the pandemic, Erin taught at the legendary and world renowned dance studio in NYC: Steps on Broadway, as well as Mark Fisher Fitness, Flywheel Sports, and at various Yoga and dance studios. She is a passionate advocate for providing accessible, pre-professional theater education to the next generation of dancers and theatremakers. "
     },
     {
         name: "Jacqueline Feiss",
         picture: <img src={JacquelineFeiss} alt="Jacqueline Feiss" />,
         bio: "Jacqueline grew up in Connecticut and has been dancing since before she could walk. She attended Roger Williams University and obtained a degree in Dance Performance. Jacqueline performed with a variety of different dance companies and studied under many of the influential dancers of today. Jacqueline has been teaching Jazz, Tap, Modern, Contemporary, Ballet, Jazz Fusion/Funk, and Hip Hop for the past twenty years and cannot imagine her life without dance! She lives in Jericho with her husband, two young daughters, and her dog. During the day, Jacqueline works as an Educational Consultant for the Vermont I-Team and helps to support full membership and inclusion for all children. When she is not dancing or consulting, she enjoys skiing, biking, and exploring all Vermont has to offer. She is looking forward to teaching Lyrical 3 at Synergy this year."
+    },
+    {
+        name: "Kate Kosharek",
+        picture: <img src={NoPicGirl} alt="Kate Kosharek" />,
+        bio: ""
     },
     {
         name: "Rose Bedard",
@@ -82,7 +99,8 @@ export const TeacherBioItems = [
         name: "Theresa Robinson",
         picture: <img src={TheresaRobinson} alt="Theresa Robinson" />,
         bio: "Theresa grew up in southern CT in a theatre family, acting, directing children’s theatre, assisting with costumes, immersing herself in all aspects of community theatre, and ALWAYS dancing along with her sisters.  She danced at Lyndon State College, running their dance club program there while majoring in Elementary Education.  She has also danced in our local community both in Burlington area and Jericho for many years. Holding a K-6 teaching certificate, Theresa ran her own preschool for more than 20 years and now is an Administrative Assistant at Browns River Middle School, just across the lawn!  Theresa has worked with children of all ages in our local community with Mia as Co-Director of Vermont Youth Dancers since 2010, and taught classes both at the Birds Nest and at Synergy.  Presently, she is teaching Creative Movement, Jazz 1 & 2, and Lyrical 2."
-    },
+    }
+/*
     {
         name: "Alex Bacheller",
         picture: <img src={AlexBacheller} alt="Alex Bacheller" />,
@@ -104,20 +122,24 @@ export const TeacherBioItems = [
         bio: ""
     },
     {
+        name: "Emma Montgomery",
+        picture: <img src={EmmaMontgomery} alt="Emma Montgomery" />,
+        bio: ""
+    },
+    {
         name: "Tanner Paffen",
         picture: <img src={TannerPaffen} alt="Tanner Paffen" />,
         bio: ""
-    }
+    },
+
+    */
 ]
-
-
-
 
 //TABioItems array - add new Teaching Assistants as objects here
 export const TABioItems = [
     {
-        name: "Audrey Rodgers, Nadia Muller",
-        picture: <img src={NoPicGirl} alt="Student 1" />,
+        name: "Alessandro Campanile, Brooklyn Belfield, Emma Peake, Vivian Ennis",
+        picture: <img src={NoPicGirl} alt="Teaching Assistants" />,
         bio: ""
     }
 ]
@@ -125,18 +147,8 @@ export const TABioItems = [
 //StudentTeacherBioItems array - add new Teaching Assistants as objects here
 export const StudentTeacherBioItems = [
     {
-        name: "Aleacia Daigle",
-        picture: <img src={NoPicGirl} alt="Aleacia Daigle" />,
-        bio: ""
-    },
-    {
-        name: "Ella Davis",
-        picture: <img src={EllaDavis} alt="Ella Davis" />,
-        bio: ""
-    },
-    {
-        name: "Ella Lavoie",
-        picture: <img src={EllaLavoie} alt="Ella Lavoie" />,
+        name: "Audry Rogers",
+        picture: <img src={NoPicGirl} alt="Audry Rogers" />,
         bio: ""
     },
     {
@@ -145,13 +157,23 @@ export const StudentTeacherBioItems = [
         bio: ""
     },
     {
-        name: "Jada Bean",
-        picture: <img src={JadaBean} alt="Jada Bean" />,
+        name: "Maeve Ennis",
+        picture: <img src={NoPicGirl} alt="Maeve Ennis" />,
         bio: ""
     },
     {
-        name: "Naia Surks",
-        picture: <img src={NaiaSurks} alt="Naia Surks" />,
+        name: "Maddie Brown",
+        picture: <img src={NoPicGirl} alt="Maddie Brown" />,
+        bio: ""
+    },
+    {
+        name: "Norah Burton",
+        picture: <img src={NoPicGirl} alt="Norah Burton" />,
+        bio: ""
+    },
+    {
+        name: "Nadia Muller",
+        picture: <img src={NoPicGirl} alt="Nadia Muller" />,
         bio: ""
     },
     {
@@ -160,14 +182,9 @@ export const StudentTeacherBioItems = [
         bio: ""
     },
     {
-        name: "Rosalie Kosnitsky",
-        picture: <img src={RosalieKosnitsky} alt="Rosalie Kosnitsky" />,
+        name: "Rivkah Anderson",
+        picture: <img src={NoPicGirl} alt="Rivkah Anderson" />,
         bio: ""
-    },
-    {
-        name: "Summer Clough",
-        picture: <img src={SummerClough} alt="Summer Clough" />,
-        bio: "Summer has been dancing for about 10 years and has been a student at Synergy since it was open! This is Summer's 3rd year student teaching at Synergy, and before that, she was a teacher's assistant to Mia Groff and April Foster. Summer takes ballet/pointe, contemporary, hip hop, and jazz classes at Synergy. Summer has also trained at the Burklyn Ballet Theatre (2019 VT) and Bolshoi Ballet Academy's (2020 & 2021 NYC) summer intensives and is also a part of the State Champion MMU Dance Team and Vermont Youth Dancers. Summer will be teaching Ballet 3A and Lyrical/Contemporary 4B this year. Summer is so excited to be a part of the Synergy staff and for 2 more years of teaching and taking classes!"
     },
     {
         name: "Taylor Gernander",
@@ -175,8 +192,8 @@ export const StudentTeacherBioItems = [
         bio: "Taylor was born and raised in Essex Vermont and has been a dancer for most of her life. Her first serious dance class was when she was 5 dancing Irish dance at McFadden Academy where she competed year round. Three years ago she realized that Irish dance wasn’t her thing and decided to take a lyrical class here at synergy! She instantly fell in love with the studio and decided to try hiphop and quickly realized she absolutely loved it! She took hiphop 5 and teen hiphop this past year and was asked to assistant teach which lead her to where she is now as a student teacher for teen hiphop! Teaching and dance has been Taylor’s dream careers for as long as she can remember so she is thrilled and honored to be able to get a taste of this life at age 14!"
     },
     {
-        name: "Zoe Papaseraphim",
-        picture: <img src={ZoePapaseraphim} alt="Zoe Papaseraphim" />,
+        name: "Zoe Merak",
+        picture: <img src={NoPicGirl} alt="Zoe Merak" />,
         bio: ""
     }
 ]
@@ -184,8 +201,8 @@ export const StudentTeacherBioItems = [
 //AdminBioItems array - add new Administrative Assistants as objects here
 export const AdminBioItems = [
     {
-        name: "Anika Romanko, Anya Muller, Mia Cannizzaro, Pilar Medina",
-        picture: <img src={NoPicGirl} alt="Student 1" />,
+        name: "Anya Muller, Rivkah Anderson, Vivian Ennis, Maddie Brown, Brooklyn Belfield",
+        picture: <img src={NoPicGirl} alt="Admin Assistants" />,
         bio: ""
     }
 ]
